2fb0ea0121f1a6e71e6ffec3f4de5b608aa58be6
EmilioDinen <emilioinformatica1@gmail.com>
2025-02-10 16:41:05 -0600

    Estructura de home

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:meca_inventory/config/dependency_injection/get_it.dart';
 import 'package:meca_inventory/domain/use_cases/get_products_use_case.dart';
 import 'package:meca_inventory/presentation/add_product/add_product_screen.dart';
+import 'package:meca_inventory/presentation/empty_products/empty_products_list.dart';
 import 'package:meca_inventory/presentation/home/blocs/get_products/get_products_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:meca_inventory/presentation/home/home_content.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,7 +16,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
   static final List<Widget> _screens = <Widget>[
-    const HomeContent(),
+    const EmptyProductsList(),
     const AddProductScreen(),
   ];
 
@@ -37,25 +37,41 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
         ),
       ],
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Meca inventario'),
-        ),
-        body: _screens[_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.list),
-              label: 'Products',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: 'Add',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-        ),
+      child: BlocBuilder<GetProductsBloc, GetProductsState>(
+        builder: (context, state) {
+          if (state is GetProductsLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is GetProductsSuccess) {
+            if (state.products.isEmpty) {
+              return const EmptyProductsList();
+            } else {
+              return Scaffold(
+                appBar: AppBar(
+                  title: const Text('Meca inventario'),
+                ),
+                body: _screens[_selectedIndex],
+                bottomNavigationBar: BottomNavigationBar(
+                  items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.list),
+                      label: 'Products',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.add),
+                      label: 'Add',
+                    ),
+                  ],
+                  currentIndex: _selectedIndex,
+                  onTap: _onItemTapped,
+                ),
+              );
+            }
+          } else if (state is GetProductsError) {
+            return Text('Error: ${state.error}');
+          } else {
+            return const SizedBox();
+          }
+        },
       ),
     );
   }
