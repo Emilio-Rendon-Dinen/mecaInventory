da684479aad2681fc71e0075d5112c45b1b627aa
Emilio-Rendon-Dinen <61218931+Emilio-Rendon-Dinen@users.noreply.github.com>
2025-08-06 16:04:14 -0600

    Se cambia BlocBuilder por BlocListener

--- lib/presentation/screens/restock_product/restock_product_screen.dart
+++ lib/presentation/screens/restock_product/restock_product_screen.dart
@@ -36,134 +36,145 @@ class _RestockProductScreenState extends State<RestockProductScreen> {
     final product = widget.product;
     return BlocProvider(
       create: (context) => EditProductBloc(),
-      child: BlocBuilder<EditProductBloc, EditProductState>(
-        builder: (context, state) {
-          if (state is EditProductLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is EditProductSuccess) {
+      child: BlocListener<EditProductBloc, EditProductState>(
+        listener: (context, state) {
+          if (state is EditProductSuccess) {
             WidgetsBinding.instance.addPostFrameCallback((_) {
               if (context.mounted) {
                 Navigation.pushAndRemoveUntil(
                   context: context,
                   screen: const HomeScreen(),
                 );
+              } else if (state is EditProductError) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Error al agregar producto'),
+                  ),
+                );
               }
             });
-            return const Center(child: Text('Stock Agregado correctamente'));
-          } else if (state is EditProductError) {
-            return const Center(child: Text('Error al agregar stock'));
-          } else {
-            return Scaffold(
-              appBar: AppBar(
-                title: Text(
-                  product.name,
-                  style:
-                      TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+          }
+        },
+        child: BlocBuilder<EditProductBloc, EditProductState>(
+          builder: (context, state) {
+            if (state is EditProductLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(
+                    product.name,
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.onPrimary),
+                  ),
+                  backgroundColor: Theme.of(context).colorScheme.primary,
                 ),
-                backgroundColor: Theme.of(context).colorScheme.primary,
-              ),
-              body: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Hero(
-                          tag: 'product-image-${product.id}',
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: product.hasImage
-                                ? Image.memory(
-                                    product.image!,
-                                    fit: BoxFit.cover,
-                                    width: MediaQuery.of(context).size.width,
-                                    height:
-                                        MediaQuery.of(context).size.width * 0.8,
-                                  )
-                                : ImagePickerButton(onImagePicked: (bytes) {}),
+                body: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Hero(
+                            tag: 'product-image-${product.id}',
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: product.hasImage
+                                  ? Image.memory(
+                                      product.image!,
+                                      fit: BoxFit.cover,
+                                      width: MediaQuery.of(context).size.width,
+                                      height:
+                                          MediaQuery.of(context).size.width *
+                                              0.8,
+                                    )
+                                  : ImagePickerButton(
+                                      onImagePicked: (bytes) {}),
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 12),
-                        Text(product.description),
-                        const SizedBox(height: 12),
-                        Form(
-                          key: _formKey,
-                          child: Column(
-                            children: [
-                              Text(
-                                  'Cantidad actual: ${product.currentQuantity}'),
-                              const SizedBox(height: 12),
-                              TextFormField(
-                                keyboardType: TextInputType.number,
-                                controller: _initialQuantityController,
-                                inputFormatters: [
-                                  FilteringTextInputFormatter.digitsOnly,
-                                ],
-                                decoration: const InputDecoration(
-                                  labelText: 'Ingresar inventario',
-                                  border: OutlineInputBorder(),
-                                ),
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Por favor ingrese un valor';
-                                  }
-                                  final quantity = num.tryParse(value);
-                                  if (quantity == null || quantity <= 0) {
-                                    return 'Por favor ingrese un número válido';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              const SizedBox(height: 20),
-                              Row(
-                                children: [
-                                  const Spacer(),
-                                  FilledButton(
-                                    onPressed: () {
-                                      final int newQuantity = int.parse(
-                                              _initialQuantityController.text) +
-                                          (product.currentQuantity ?? 0);
-                                      if (_formKey.currentState!.validate()) {
-                                        final tempProduct = ProductUIModel(
-                                          id: product.id,
-                                          name: product.name,
-                                          description: product.description,
-                                          cost: product.cost,
-                                          initialQuantity: (newQuantity >
-                                                  product.initialQuantity)
-                                              ? newQuantity
-                                              : product.initialQuantity,
-                                          currentQuantity: newQuantity,
-                                          categoryId: product.categoryId,
-                                          image: product.image,
-                                          hasImage: product.hasImage,
-                                        );
-
-                                        context.read<EditProductBloc>().add(
-                                              EditProductRequestedEvent(
-                                                productUIModel: tempProduct,
-                                                useCase: getIt
-                                                    .get<EditProductUseCase>(),
-                                              ),
-                                            );
-                                      }
-                                    },
-                                    child: const Text('Guardar'),
+                          const SizedBox(height: 12),
+                          Text(product.description),
+                          const SizedBox(height: 12),
+                          Form(
+                            key: _formKey,
+                            child: Column(
+                              children: [
+                                Text(
+                                    'Cantidad actual: ${product.currentQuantity}'),
+                                const SizedBox(height: 12),
+                                TextFormField(
+                                  keyboardType: TextInputType.number,
+                                  controller: _initialQuantityController,
+                                  inputFormatters: [
+                                    FilteringTextInputFormatter.digitsOnly,
+                                  ],
+                                  decoration: const InputDecoration(
+                                    labelText: 'Ingresar inventario',
+                                    border: OutlineInputBorder(),
                                   ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Por favor ingrese un valor';
+                                    }
+                                    final quantity = num.tryParse(value);
+                                    if (quantity == null || quantity <= 0) {
+                                      return 'Por favor ingrese un número válido';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                const SizedBox(height: 20),
+                                Row(
+                                  children: [
+                                    const Spacer(),
+                                    FilledButton(
+                                      onPressed: () {
+                                        final int newQuantity = int.parse(
+                                                _initialQuantityController
+                                                    .text) +
+                                            (product.currentQuantity ?? 0);
+                                        if (_formKey.currentState!.validate()) {
+                                          final tempProduct = ProductUIModel(
+                                            id: product.id,
+                                            name: product.name,
+                                            description: product.description,
+                                            cost: product.cost,
+                                            initialQuantity: (newQuantity >
+                                                    product.initialQuantity)
+                                                ? newQuantity
+                                                : product.initialQuantity,
+                                            currentQuantity: newQuantity,
+                                            categoryId: product.categoryId,
+                                            image: product.image,
+                                            hasImage: product.hasImage,
+                                          );
+
+                                          context.read<EditProductBloc>().add(
+                                                EditProductRequestedEvent(
+                                                  productUIModel: tempProduct,
+                                                  useCase: getIt.get<
+                                                      EditProductUseCase>(),
+                                                ),
+                                              );
+                                        }
+                                      },
+                                      child: const Text('Guardar'),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            );
-          }
-        },
+              );
+            }
+          },
+        ),
       ),
     );
   }
